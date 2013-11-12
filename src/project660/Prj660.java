2a283a8a84a4565f6f1aff99c4730bca30bdb2b7
Mike Gordo <himor.cre@gmail.com>
2013-11-11 21:07:05 -0500

    Project 660

---
+++ src/project660/Prj660.java
@@ -0,0 +1,12 @@
+package project660;
+
+public class Prj660 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
